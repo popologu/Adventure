6f0a6f824f53d2a4a1e09986bd242c44dd5ab2f9
popologu <popologu@gmail.com>
2018-05-31 09:39:27 +0900

    커밋~

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -3,29 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	
+	[Header("Visual")]
+	public GameObject model;
+
+	[Header("Movement")]
+	public float movingVelocity;
+	public float jumpingVelocity;
+
+	private Rigidbody playerRigidbody;
+	private bool canJump = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		playerRigidbody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+		RaycastHit hit;
+	 	if	(Physics.Raycast(transform.position, Vector3.down, out hit, 1f)) {
+			 canJump = true;
+	 	}
+
+		ProcessInput ();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if(collision.transform.tag == "Floor") {
+			canJump = true;
+		}
+	}
+
+	void ProcessInput () {
+			playerRigidbody.velocity = new Vector3 (
+			0,
+			playerRigidbody.velocity.y,
+			0
+		);
+
 		if (Input.GetKey("right")) {
-			transform.position += Vector3.right * 5f * Time.deltaTime;
+				playerRigidbody.velocity = new Vector3 (
+				movingVelocity,
+				playerRigidbody.velocity.y,
+				playerRigidbody.velocity.z
+			);
+
+			model.transform.localEulerAngles = new Vector3 (0,270,0);
 		}
 		if (Input.GetKey("left")) {
-			transform.position += Vector3.left * 5f * Time.deltaTime;
+				playerRigidbody.velocity = new Vector3 (
+				-movingVelocity,
+				playerRigidbody.velocity.y,
+				playerRigidbody.velocity.z
+			);
+			model.transform.localEulerAngles = new Vector3 (0,90,0);
 		}
 		if (Input.GetKey("up")) {
-			transform.position += Vector3.forward * 5f * Time.deltaTime;
+				playerRigidbody.velocity = new Vector3 (
+				playerRigidbody.velocity.x,
+				playerRigidbody.velocity.y,
+				movingVelocity
+			);
+			model.transform.localEulerAngles = new Vector3 (0,180,0);
 		}
 		if (Input.GetKey("down")) {
-			transform.position += Vector3.back * 5f * Time.deltaTime;
-		}						
-
-		
-	
+				playerRigidbody.velocity = new Vector3 (
+				playerRigidbody.velocity.x,
+				playerRigidbody.velocity.y,
+				-movingVelocity
+			);
+			model.transform.localEulerAngles = new Vector3 (0,0,0);
+		}
+		if (Input.GetKeyDown("space") && canJump) {
+			canJump = false;
+			// GetComponent<Rigidbody> ().AddForce(0, jumpingVelocity,0);
+				playerRigidbody.velocity = new Vector3 (
+				playerRigidbody.velocity.x,
+				jumpingVelocity,
+				playerRigidbody.velocity.z
+			);
+		}
 	}
 }
   
